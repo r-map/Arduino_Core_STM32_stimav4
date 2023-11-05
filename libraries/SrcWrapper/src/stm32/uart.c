37ec752b9084933f4fa45e227f99c7097299026f
Paolo Patruno <p.patruno@iperbole.bologna.it>
2023-11-05 12:33:55 +0100

    define USART2_IRQHandler as weak

--- libraries/SrcWrapper/src/stm32/uart.c
+++ libraries/SrcWrapper/src/stm32/uart.c
@@ -947,7 +947,7 @@ void USART1_IRQHandler(void)
   * @retval None
   */
 #if defined(USART2_BASE)
-void USART2_IRQHandler(void)
+__weak void USART2_IRQHandler(void)
 {
   HAL_NVIC_ClearPendingIRQ(USART2_IRQn);
   if (uart_handlers[UART2_INDEX] != NULL) {
